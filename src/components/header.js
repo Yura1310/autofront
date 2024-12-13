7a00f564d58cf7ee278bd2d3193a0f660b96d767
Yura1310 <103391854+Yura1310@users.noreply.github.com>
2024-12-13 15:20:41 +1000

    1

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,15 @@
 function Header() {
     return (
-        <header class="bg-[#f97316] text-white p-4">
-            <div class="container mx-auto flex justify-between items-center">
-            <h1 class="text-xl font-bold">Лого</h1>
-            <nav class="space-x-3 hidden sm:flex">
-            <a href="#" class="hover:underline">о нас</a>
-            <a href="#" class="hover:underline">услуги</a>
-            <a href="#" class="hover:underline">наши проекты</a>
-            <a href="#" class="hover:underline">запись на обучение</a>
+        <header class="bg-[#201F1F] text-white p-4">
+            <div class="container mx-auto ">
+            <nav class="space-x-40 hidden sm:flex justify-center" >
+              <img src="../images/EV cehdbc_.png" class=""></img>
+              <a href="#"> Добро пожаловать!</a>
+              <a href="#" class="hover:underline">о нас</a>
+              
+              <a href="#" class="hover:underline">услуги</a>
+              <a href="#" class="hover:underline">наши проекты</a>
+              <a href="#" class="hover:underline">запись на обучение</a>
           </nav>
         </div>
       </header>
